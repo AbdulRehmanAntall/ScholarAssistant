b64cd7ca05f19961cf62fe287b85ef5c14cb1c5d
AbdulRehmanAntall <abdulrehmanantall420@gmail.com>
2025-10-16 03:07:25 +0500

    Optimized the file structure

--- Backend/main.py
+++ Backend/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from app.api.pdf_routes import router as pdf_router
 from app.api.citation_routes import router as citation_router
 from app.api.query_routes import router as query_router
-
+from app.api.auth_routes import router as auth_router
 app = FastAPI()
 app.include_router(pdf_router)
 app.include_router(citation_router)
 app.include_router(query_router)
+app.include_router(auth_router)
 
 
 @app.get("/")
